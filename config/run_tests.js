04949f7e218a0ad49bb9dc410526398b90889e43
icontreras <israel.contreras@lastminute.com>
2020-02-21 11:29:38 +0100

    New reporter.

--- config/run_tests.js
+++ config/run_tests.js
@@ -1,6 +1,12 @@
 import Jasmine from 'jasmine'
+import { SpecReporter } from 'jasmine-spec-reporter';
 
 const jasmine = new Jasmine();
+const reporter = new SpecReporter();
 
+console.clear();
+
+jasmine.env.clearReporters();
+jasmine.env.addReporter(reporter);
 jasmine.loadConfigFile('./jasmine.json');
 jasmine.execute();
---
+++ test/domain/sayILoveYou.spec.js
@@ -0,0 +1,12 @@
+describe('I want to say I love you', () => {
+    describe('When I feel love', () => {
+        it('to say I love you ', function () {
+
+        });
+    });
+    describe('When I feel secure ', () => {
+        it('to say I love you ', function () {
+
+        });
+    });
+});
